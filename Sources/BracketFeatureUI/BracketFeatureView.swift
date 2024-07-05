701e7bfa93237b69b7e6bfddcc4a25a25ba73483
Connor Neville <connor.neville16@gmail.com>
2024-07-05 14:29:42 -0400

    BracketFeatureView.swift UI iteration

--- Sources/BracketFeatureUI/BracketFeatureView.swift
+++ Sources/BracketFeatureUI/BracketFeatureView.swift
@@ -1,3 +1,5 @@
+import API
+import APIExtensions
 import BracketFeatureKit
 import ComposableArchitecture
 import SwiftUI
@@ -13,23 +15,89 @@ public struct BracketFeatureView: View {
     public var body: some View {
         NavigationStack {
             ScrollView {
-//                if let winnersMatches = store.groupedMatches[.winners] {
-//                    Section("Winners Bracket") {
-//                        ForEach(winnersMatches) { <#Identifiable#> in
-//                            <#code#>
-//                        }
-//
-//                        Button { } label: {
-//                            RoundedRectangle(cornerRadius: 24)
-//                                .frame(width: 600, height: 240)
-//                                .background(.blue)
-//                        }
-//                    }
-//                    .frame(maxWidth: .infinity)
-//
-//                }
+                if let winnersMatches = store.groupedMatches[.winners] {
+                    bracketSide(
+                        groupedMatches: winnersMatches,
+                        title: "Winners’ Bracket"
+                    )
+                }
+                if let losersMatches = store.groupedMatches[.losers] {
+                    bracketSide(
+                        groupedMatches: losersMatches,
+                        title: "We’re All Winners Bracket"
+                    )
+                }
             }
             .navigationTitle(store.bracketName)
+        }
+    }
+
+}
+
+private extension BracketFeatureView {
+
+    func bracketSide(
+        groupedMatches: [Round: IdentifiedArrayOf<Match>],
+        title: String
+    ) -> some View {
+        Section(title) {
+            HStack(alignment: .top) {
+                ForEach(Array(groupedMatches.keys).sorted(), id: \.self) { round in
+                    VStack {
+                        let matches = groupedMatches[round]!
+                        Text("Round \(round)")
+                        ForEach(matches) { match in
+                            Button { } label: {
+                                matchView(match: match)
+                            }
+                        }
+                        Spacer()
+                    }
+                    .containerRelativeFrame([.horizontal, .vertical]) { value, axis in
+                        switch axis {
+                        case .horizontal:
+                            value / 4
+                        case .vertical:
+                            value * 3 / 4
+                        }
+                    }
+                    .border(.red)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+
+    func matchView(
+        match: Match
+    ) -> some View {
+        VStack(spacing: 8) {
+            participantLabel(match.participant1)
+                .font(.headline)
+            Text("VS")
+                .font(.caption)
+            participantLabel(match.participant2)
+                .font(.headline)
+        }
+        .frame(maxWidth: .infinity)
+    }
+
+    func participantLabel(
+        _ participant: MatchParticipant
+    ) -> some View {
+        HStack(spacing: 8) {
+            switch participant {
+            case let .participant(participant):
+                participant.country.bundleAsset
+                    .resizable()
+                    .frame(width: 60, height: 40, alignment: .bottomTrailing)
+                Text(participant.name)
+            case let .awaitingWinner(matchNumber):
+                Text("Winner of #\(matchNumber)")
+            case let .awaitingLoser(matchNumber):
+                Text("Loser of #\(matchNumber)")
+            }
+            Spacer()
         }
     }
 
