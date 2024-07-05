959bf137ae55d90acaefa5301d413f1f9c483192
Connor Neville <connor.neville16@gmail.com>
2024-07-05 14:00:15 -0400

    Use JSON on file instead of API for all brackets

--- Sources/APIClients/GetAllBracketsAPIClient.swift
+++ Sources/APIClients/GetAllBracketsAPIClient.swift
@@ -1,7 +1,9 @@
 import API
 import Dependencies
 import DependenciesMacros
+import Foundation
 import GenericAPIClient
+import IdentifiedCollections
 
 @DependencyClient public struct GetAllBracketsAPIClient {
 
@@ -23,8 +25,15 @@ extension GetAllBracketsAPIClient: DependencyKey {
     public static var liveValue: GetAllBracketsAPIClient {
         GetAllBracketsAPIClient(
             get: {
-                try await genericAPIClient.fetch(
-                    route: .getAllBrackets
+                let json = """
+[{"createdAt":"2024-07-05T14:00:18Z","matches":[{"kind":{"default":{}},"matchNumber":1,"participant1":{"participant":{"_0":{"countryID":"LA","name":"Liam"}}},"participant2":{"participant":{"_0":{"countryID":"MT","name":"Jan"}}},"round":0,"side":{"winners":{}}},{"kind":{"default":{}},"matchNumber":3,"participant1":{"participant":{"_0":{"countryID":"DO","name":"Liz"}}},"participant2":{"participant":{"_0":{"countryID":"PA","name":"Kelly"}}},"round":1,"side":{"winners":{}}},{"kind":{"default":{}},"matchNumber":4,"participant1":{"participant":{"_0":{"countryID":"FJ","name":"Drake"}}},"participant2":{"participant":{"_0":{"countryID":"SC","name":"Cody"}}},"round":1,"side":{"winners":{}}},{"kind":{"default":{}},"matchNumber":5,"participant1":{"participant":{"_0":{"countryID":"IN","name":"Connor"}}},"participant2":{"participant":{"_0":{"countryID":"GE","name":"Joe"}}},"round":1,"side":{"winners":{}}}],"name":"7/5/24"}]
+
+"""
+                return .init(
+                    brackets: try JSONDecoder.api.decode(
+                        IdentifiedArrayOf<Bracket>.self,
+                        from: json.data(using: .utf8)!
+                    )
                 )
             }
         )
