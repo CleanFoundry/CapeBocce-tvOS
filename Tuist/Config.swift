40c0c8aab5a82a4169ee0d9b3f9f20dd192891aa
Connor Neville <connor.neville16@gmail.com>
2024-07-12 14:58:17 -0400

    Building and running with Xcode 16b3

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,12 +1,12 @@
 import ProjectDescription
 
 let config = Config(
-    compatibleXcodeVersions: .exact(.init(15, 3, 0)),
+    compatibleXcodeVersions: .exact(.init(16, 0, 0)),
     swiftVersion: .init(5, 9, 0),
     plugins: [
         .git(
             url: "git@github.com:CleanFoundry/cf-tuist-helpers.git",
-            tag: Version(0, 0, 14).description
+            tag: Version(0, 0, 16).description
         )
     ]
 )
--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,15 +1,15 @@
 // swift-tools-version: 5.9
-import PackageDescription
+@preconcurrency import PackageDescription
 
 #if TUIST
-    import ProjectDescription
-
-    let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,] 
-        productTypes: [:]
-    )
+//    import ProjectDescription
+//
+//    let packageSettings = PackageSettings(
+//        // Customize the product types for specific package product
+//        // Default is .staticFramework
+//        // productTypes: ["Alamofire": .framework,] 
+//        productTypes: [:]
+//    )
 #endif
 
 let package = Package(
