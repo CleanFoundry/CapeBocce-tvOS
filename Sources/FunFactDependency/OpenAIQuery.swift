0fbcd2e73c5d0985f82cd9a3b8fdc6af25b73635
Connor Neville <connor.neville16@gmail.com>
2024-08-05 13:13:36 -0400

    Typo in GPT

--- Sources/FunFactDependency/OpenAIQuery.swift
+++ Sources/FunFactDependency/OpenAIQuery.swift
@@ -14,7 +14,7 @@ struct OpenAIQuery: Encodable {
             messages: [
                 .init(
                     role: "system",
-                    content: "You will be provided names of countries. When given the name of a coutry, look up a fun fact about that country. The fun fact should be interesting, family friendly, and at most 1 short paragraph. Do not include any other text besides the fun fact."
+                    content: "You will be provided names of countries. When given the name of a country, look up a fun fact about that country. The fun fact should be interesting, family friendly, and at most 1 short paragraph. Do not include any other text besides the fun fact."
                 ),
                 .init(
                     role: "user",
